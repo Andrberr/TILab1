1e771962e4fdeb951558cf583665c28645119493
BABASTAS728 <Aberezkin954@gmail.com>
2023-02-18 14:56:02 +0300

    commit 2

--- app/src/main/java/com/example/ti1/MainActivity.kt
+++ app/src/main/java/com/example/ti1/MainActivity.kt
@@ -46,31 +46,64 @@ class MainActivity : AppCompatActivity() {
         'я'
     )
 
+    private val matrix = getMatrix()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val sourceTextView = findViewById<EditText>(R.id.sourceText)
+        val encryptedTextView = findViewById<EditText>(R.id.encryptedText)
         val keyView = findViewById<EditText>(R.id.key)
         val cypherTextView = findViewById<TextView>(R.id.cypherText)
+        val decryptionTextView = findViewById<TextView>(R.id.decryptionText)
 
         val sourceTextViewV = findViewById<EditText>(R.id.sourceTextV)
+        val encryptedTextViewV = findViewById<EditText>(R.id.encryptedTextV)
         val keyViewV = findViewById<EditText>(R.id.keyV)
         val cypherTextViewV = findViewById<TextView>(R.id.cypherTextV)
-
+        val decryptionTextViewV = findViewById<TextView>(R.id.decryptionTextV)
 
         findViewById<Button>(R.id.cypherButton).setOnClickListener {
-            var sourceText = sourceTextView.text.toString()
-            val key = keyView.text.toString().toInt()
+            var sourceText = getStringInput(sourceTextView.text.toString())
+            val strKey = keyView.text.toString()
+            if (strKey.isNotEmpty()) {
+                val key = strKey.toInt()
+                if (checkForMutuallySimple(key)) {
+                    cypherTextView.text = getCypherText(sourceText, key)
+                } else Toast.makeText(this, "k и n не взаимно простые!!!", Toast.LENGTH_LONG).show()
+            } else Toast.makeText(this, "Поле с ключом пустое!!!", Toast.LENGTH_LONG).show()
 
-            if (checkForMutuallySimple(key)) {
-                cypherTextView.text = getCypherText(sourceText, key)
-            } else Toast.makeText(this, "k и n не взаимно простые!!!", Toast.LENGTH_LONG).show()
-
-            sourceText = sourceTextViewV.text.toString()
-            val keyV = keyViewV.text.toString()
+            sourceText = getStringInput(sourceTextViewV.text.toString())
+            val keyV = getStringInput(keyViewV.text.toString())
             cypherTextViewV.text = getCypherTextV(sourceText, keyV)
         }
+
+        findViewById<Button>(R.id.decryptionButton).setOnClickListener {
+            var encryptedText = getStringInput(encryptedTextView.text.toString())
+            val strKey = keyView.text.toString()
+
+            if (strKey.isNotEmpty()) {
+                val key = strKey.toInt()
+                if (checkForMutuallySimple(key)) {
+                    decryptionTextView.text = getDecryptionText(encryptedText, key)
+                } else Toast.makeText(this, "k и n не взаимно простые!!!", Toast.LENGTH_LONG).show()
+            } else Toast.makeText(this, "Поле с ключом пустое!!!", Toast.LENGTH_LONG).show()
+
+            encryptedText = getStringInput(encryptedTextViewV.text.toString())
+            val keyV = getStringInput(keyViewV.text.toString())
+            decryptionTextViewV.text = getDecryptionTextV(encryptedText, keyV)
+        }
+    }
+
+    private fun getStringInput(text: String): String {
+        var input = ""
+        for (i in text.indices) {
+            if (alphabet.indexOf(text[i]) != -1 || (text[i] in 'А'..'Я') || text[i] == 'Ё') {
+                input += text[i].lowercase()
+            }
+        }
+        return input
     }
 
     private fun checkForMutuallySimple(k: Int): Boolean {
@@ -94,7 +127,15 @@ class MainActivity : AppCompatActivity() {
         return result
     }
 
-    private fun getCypherTextV(sourceText: String, key: String): String {
+    private fun getDecryptionText(cypherText: String, key: Int): String {
+        var result = ""
+        for (element in cypherText) {
+            result += alphabet[(alphabet.indexOf(element) / key) % n]
+        }
+        return result
+    }
+
+    private fun getMatrix(): Array<Array<Char>> {
         val matrix = Array(n) { Array(n) { ' ' } }
         for (i: Int in 0 until n) {
             var j = i
@@ -112,7 +153,10 @@ class MainActivity : AppCompatActivity() {
                 k++
             }
         }
+        return matrix
+    }
 
+    private fun getCypherTextV(sourceText: String, key: String): String {
         val table = Array(2) { Array(sourceText.length) { ' ' } }
         for (i in sourceText.indices) {
             table[0][i] = sourceText[i]
@@ -122,6 +166,19 @@ class MainActivity : AppCompatActivity() {
         var result = ""
         for (i in sourceText.indices) {
             result += matrix[alphabet.indexOf(table[1][i])][alphabet.indexOf(table[0][i])]
+        }
+        return result
+    }
+
+    private fun getDecryptionTextV(cypherText: String, key: String): String {
+        var result = ""
+        for (i in cypherText.indices) {
+            val ind = alphabet.indexOf(key[i % key.length])
+            for (j in alphabet.indices) {
+                if (matrix[ind][j] == cypherText[i]) {
+                    result += alphabet[j]
+                }
+            }
         }
         return result
     }
